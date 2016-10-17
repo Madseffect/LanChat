f3ba8ca1894de22f313c13cc91caa6bb74ecb5d7
RunningAroundIC <karsten@raic.dk>
2016-10-17 15:14:07 +0200

    Tester velommens tekst

---
+++ LanChat/LanChat/LoginHaandtering.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanChat
+{
+    class LoginHaandtering
+    {
+
+        string bruger;
+        string operationInput;
+
+        public void VelkommenTekst()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Venligst angiv dit bruger navn!");
+            bruger = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Dit bruger navn er: " + bruger);
+            Console.WriteLine("");
+            Console.WriteLine("Vil du oprette forbindelse til LanChat Serveren? j/n");
+        }
+
+        public void BrugerHaandtering()
+        {
+            while (true)
+            {
+                if (Console.ReadLine() == "n")
+                {
+                    VelkommenTekst();
+                }
+
+                if (Console.ReadLine() == "j")
+                {
+                    Console.WriteLine("Opretter forbindelse til server!");
+                    break;
+                }
+
+            }
+        }
+
+    }
+}
--- LanChat/LanChat/Program.cs
+++ LanChat/LanChat/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LanChat
-{
+{ 
     class Program
     {
+
         static void Main(string[] args)
         {
+
+            // LanChatClient    
+
+            LoginHaandtering login = new LoginHaandtering();
+
+            //Bruger
+
+            Console.WriteLine("Velkommen til LanChat!");
+          
+            login.VelkommenTekst();
+
+            login.BrugerHaandtering();
+
+
+
+            // Server forbindelse
+            TcpClient clientSocket = new TcpClient("127.0.0.1",6789);
+
+            Stream gs = clientSocket.GetStream();
+            StreamReader streamReader = new StreamReader(gs);
+            StreamWriter streamWriter = new StreamWriter(gs);
+            streamWriter.AutoFlush = true;
+            
+
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                streamWriter.WriteLine(message);
+
+
+                
+
+
+
+                if (message == "STOP CHAT") break;    
+                
+            }
+
+
+            gs.Close();
+
+            clientSocket.Close();
+
         }
     }
 }
