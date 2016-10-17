b82b8643cc08decbd3e575affc29e4f748c163cb
MadsEffect <thema@DESKTOP-BB9V30M>
2016-10-17 14:11:13 +0200

    SERVER IS DONE

--- LanChat/LanChat/Program.cs
+++ LanChat/LanChat/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,66 @@ namespace LanChat
 {
     class Program
     {
+        public static Hashtable clientsList= new Hashtable();
         static void Main(string[] args)
         {
+            TcpListener serverSocket= new TcpListener(8888);
+            TcpClient clientSocket = new TcpClient();
+            int counter = 0;
+
+            serverSocket.Start();
+            Console.WriteLine("LanChatServer er startet!");
+            counter = 0;
+            while (true)
+            {
+                counter = +1;
+                clientSocket = serverSocket.AcceptTcpClient();
+
+                byte[] bytesFrom = new byte[10025];
+                string dataFromClient = null;
+
+                NetworkStream networkStream = clientSocket.GetStream();
+                networkStream.Read(bytesFrom, 0, (int) clientSocket.ReceiveBufferSize);
+                dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                clientsList.Add(dataFromClient,clientSocket);
+
+                broadcast(dataFromClient + " Joined ", dataFromClient, false);
+
+                Console.WriteLine(dataFromClient + "Joined chat room");
+                handleClient client = new handleClient();
+                client.startClient(clientSocket, dataFromClient, clientsList);
+            }
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine("exit");
+            Console.ReadLine();
+        }
+
+        public static void broadcast(string msg, string uName, bool flag)
+        {
+            foreach (DictionaryEntry item in clientsList)
+            {
+                TcpClient broadcastSocket;
+                broadcastSocket = (TcpClient) item.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+                Byte[] broadcastBytes = null;
+
+                if (flag==true)
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(uName + " says: " + msg);
+                }
+                else
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(msg);
+                }
+
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
+            }
         }
     }
 }
+
+
---
+++ LanChat/LanChat/handleClient.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LanChat
+{
+    public class handleClient
+    {
+        TcpClient clientSocket;
+        string clNo;
+        Hashtable clientsList;
+
+        public void startClient(TcpClient inClientSocket, string clientNo, Hashtable cList)
+        {
+            clientSocket = inClientSocket;
+            clNo = clientNo;
+            clientsList = cList;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom= new byte[10025];
+            string dataFromClient = null;
+            byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+
+            try
+
+            {
+
+                requestCount = requestCount + 1;
+
+                NetworkStream networkStream = clientSocket.GetStream();
+
+                networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                Console.WriteLine("From client - " + clNo + " : " + dataFromClient);
+
+                rCount = Convert.ToString(requestCount);
+
+
+
+                Program.broadcast(dataFromClient, clNo, true);
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                Console.WriteLine(ex.ToString());
+
+            }
+        }
+    }
+}
