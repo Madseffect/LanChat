f7d6d334259176872a11154632e26f44655437ce
MadsEffect <thema@DESKTOP-BB9V30M>
2016-10-18 09:56:48 +0200

    removed redundant code

--- LanChat/LanChat/Program.cs
+++ LanChat/LanChat/Program.cs
@@ -12,30 +12,7 @@ namespace LanChat
     {
         static void Main(string[] args)
         {
-            //MailMessage mail= new MailMessage("fjollefar@lort.dk","geert@tsz.dk");
-            //SmtpClient client = new SmtpClient();
-            //client.Port = 25;
-            //client.DeliveryMethod=SmtpDeliveryMethod.Network;
-            //client.UseDefaultCredentials = false;
-            //client.Host = "smtp.google.com";
-            //mail.Subject = "Test til Karsten mail";
-            //mail.Body = "Hello im a nigerian prince <3";
-            //client.Send(mail);
-
-            //SmtpClient client = new SmtpClient();
-            //client.Port = 80;
-            //client.Host = "smtp.gmail.com";
-            //client.EnableSsl = true;
-            //client.Timeout = 10000;
-            //client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            //client.UseDefaultCredentials = false;
-            //client.Credentials = new System.Net.NetworkCredential
-
-            //MailMessage mm = new MailMessage("themadsk@gmail.com", "themadsk@gmail.com", "test", "test");
-            //mm.BodyEncoding = UTF8Encoding.UTF8;
-            //mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
-
-            //client.Send(mm);
+           
 
             MailMessage mail = new MailMessage();
             SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
@@ -43,7 +20,7 @@ namespace LanChat
             while (true)
             {
                 mail.From = new MailAddress("lorte@lorte.dk");
-                mail.To.Add("geert@tsz.dk");
+                mail.To.Add("mikimoller@gmail.com");
                 mail.Subject = "Hello Mail";
                 mail.Body = "Hello this is the nigerian prince!";
 
